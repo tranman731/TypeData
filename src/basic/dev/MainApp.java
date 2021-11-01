e57024aba5967dd4ea301f12a8a1adca73cb62b4
Man <tranman731@gmail.com>
2021-11-01 14:25:27 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,68 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		//Thông tin một sinh viên
+		String firstName = "Tran";
+		String lastName = "Man";
+		String fullName = "Tran Man";
+		int old = 18 ;
+		String id = "2160167" ;
+		String cmnd = "046203013272" ;
+		String School = "caodangCongnghiephue" ;
+		String Locate = "TDP Hai tien thuan an phu vang thua thien hue";
+		String lop = "K21CDTH41" ;
+		String SDT = "0364890450";
+		String HANG = "mercedes" ;
+		String COLOR = "Black";
+		int BUY = 9000 ;
+		int HSL = 12345 ;
+		float markMath = 9f ;
+		boolean passed = true ;
+		
+		System.out.println("Ho va ten");
+		System.out.println(firstName + "" +lastName);
+		System.out.println("Điểm trung bình : "+ markMath);
+		System.out.println("Mã sinh viên:" + id );
+		System.out.println("Chứng minh nhân dân:" + cmnd);
+		System.out.println("Trường:" + School);
+		System.out.println("Lớp:" +lop );
+		System.out.println("Passed:" + passed);
+		System.out.println("");
+	    System.out.println("thong tin cong nhan:");
+	    
+	    //* Thông tin công nhân
+	    System.out.println("Tên:" + fullName);
+	    System.out.println("Địa chỉ:" + Locate);
+	    System.out.println("Số Điện Thoại" + SDT);
+	    System.out.println("Hệ số lương:" + HSL);
+	    System.out.println("");
+	    System.out.println("thong tin mot chiec xe:");
+	    
+	    //* Thông tin một chiếc xe car
+	    System.out.println("Ten:" + fullName);
+	    System.out.println("Hãng:" + HANG);
+	    System.out.println("Giá:" + BUY+"$");
+	    System.out.println("Màu Sơn:" + COLOR);
+	    
+	    
+	    
+	    
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
